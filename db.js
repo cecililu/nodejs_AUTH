d3e3ab1ad1fa0fd042955791b187cd10859bb81a
cecil <cecilghimire@gmail.com>
2022-08-31 19:26:33 +0545

    db Connection set up

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose=require("mongoose")
 require('dotenv').config()
 const dbconnect=async()=>{
-    mongoose.connect('mongodb+srv://cecilghi:<>@cluster0.58v39nr.mongodb.net/test'.replace('<>',process.env.DB_PASS)).then(()=>console.log("SUCESSS")).catch(()=>console.log("DB_FAILURE"))  
+    mongoose.connect('mongodb+srv://cecilghi:<>@cluster0.58v39nr.mongodb.net/test'.replace('<>',process.env.DB_PASS)).
+    then(()=>console.log("SUCESSFULL CONNECTED TO DATABASE"))
+    .catch(()=>console.log("DB_CONNECTION_FAILURE"))  
 }
 
 module.exports=dbconnect
