1c83bd54a90480f905f2d735f0b2cd2a5acdc6cc
cecil <cecilghimire@gmail.com>
2022-09-01 22:24:09 +0545

    register route bug findone

--- routes/auth.js
+++ routes/auth.js
@@ -3,14 +3,26 @@ const router= require("express").Router()
 const User=require("../models/User")
 
 router.post('/register',async(req,res)=>{
-      
+      const alreadyExist=User.findOne(req.body.email)
+      console.log(alreadyExist)
+       if (!alreadyExist){
         const user = new User({ 
         name :req.body.name,
         email: req.body.email,
-        password:req.body.password
+        password:req.body.password,  
       })
-        const a= await user.save()
-        res.send(a)
+      try{
+      const usersaved = await user.save();
+      res.send(usersaved);
+      }catch(e){
+       console.log('user could not be saved',e)
+       res.send(err)
+      }
+    }
+     else {
+      res.send("An account with that email already exists")
+     }
+        
         
 
 
