cac50fb612155b4f268a519f0407f7a90101e0b6
cecil <cecilghimire@gmail.com>
2022-09-02 22:17:51 +0545

    validation

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,29 @@ const router= require("express").Router()
 
 const User=require("../models/User")
 
+//validation 
+// const joi=require('@hapi/joi')
+const Joi = require("@hapi/joi")
+
+const schema={
+  name :Joi.string()
+      .min(6).required(),
+  email: Joi.string().
+      min(6).required()
+      .email(),
+  password:Joi.string()
+      .min(6).
+      required().
+      email(),
+}
+
 router.post('/register',async(req,res)=>{
-      const alreadyExist=User.findOne(req.body.email)
-      console.log(alreadyExist)
-       if (!alreadyExist){
+      // const alreadyExist=User.findOne(req.body.email)
+      // console.log(alreadyExist)
+       
+      //validating the data
+        // const validatation=Joi.validate(req.body,schema)
+        // res.send(validatation)
         const user = new User({ 
         name :req.body.name,
         email: req.body.email,
@@ -14,14 +33,12 @@ router.post('/register',async(req,res)=>{
       try{
       const usersaved = await user.save();
       res.send(usersaved);
-      }catch(e){
-       console.log('user could not be saved',e)
-       res.send(err)
       }
-    }
-     else {
-      res.send("An account with that email already exists")
-     }
+      catch(e){
+        console.log('user could not be saved',e)
+       res.send(err) 
+      }
+     
         
         
 
